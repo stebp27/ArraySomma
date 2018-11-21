6a3f5c0a779ce8c6bf50fb8137eab4801f9ba458
stebp27 <45163708+stebp27@users.noreply.github.com>
2018-11-21 11:11:01 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ArraySomma
+{
+    public class Program
+    {
+        static void Main(string[] args){ }
+
+        public static bool SumIsInside(int[] array, int x)
+        {
+            int sum = 0;
+
+            for (int j = array.Length - 1; j > 0; j--)
+            {
+
+                for (int i = 0; i < j; i++)
+                {
+                    if (array[i] >= x)
+                    {
+                        return false;
+                    }
+
+                    sum = array[i] + array[j];
+
+                    if (sum > x)
+                        break;
+
+                    if (sum == x)
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
